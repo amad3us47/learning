944ae64045f8e04b524502514e38c1e2a1eefd3b
amad3us47 <markus39107@gmail.com>
2024-12-06 08:45:04 -0500

    d

---
+++ lisp/chap4/inractprmpt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+static char input[2048];
+int main(int argc,char** argv)
+{
+	//printf("%ld",sizeof(input));
+	/* Print Version and Exit Information*/
+	puts("Lisp Version 0.0.0.1");
+	puts("Press Ctrl+c to Exit\n");
+	/*In a never ending loop*/
+	while(1)
+	{
+
+	}
+	return 0;
+}
